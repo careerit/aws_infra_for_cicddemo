d03e8541398e4fa6c14a9e3d7fee4cb1dbb451e1
KK <kiranops@yahoo.com>
2022-07-08 07:13:31 +0530

    nat gateway instead of gateway

--- routetables.tf
+++ routetables.tf
@@ -3,7 +3,7 @@ resource "aws_route_table" "pubroute" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    ngateway_id = aws_internet_gateway.myigw.id
+    gateway_id = aws_internet_gateway.myigw.id
   }
 
   tags = {
